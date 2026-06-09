8b2e6a5837145ea45e386441fc80865b9205a56c
perish95 <perish95@naver.com>
2026-06-09 10:46:57 +0900

    260609 RESTful 예제2

--- ex05_rest_api/src/main/java/com/example/ex05_restapi/controller/MemberApiController.java
+++ ex05_rest_api/src/main/java/com/example/ex05_restapi/controller/MemberApiController.java
@@ -1,12 +1,19 @@
 package com.example.ex05_restapi.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient.ResponseSpec;
 
 import com.example.ex05_restapi.dto.MemberRequest;
 import com.example.ex05_restapi.dto.MemberResponse;
@@ -27,5 +34,54 @@ public class MemberApiController {
   public ResponseEntity<MemberResponse> createMember(@RequestBody MemberRequest request) {
     MemberResponse savedMember = memberService.save(request);
     return ResponseEntity.status(HttpStatus.CREATED).body(savedMember);
+  }
+
+  // 2. 전체 조회
+  @GetMapping
+  public ResponseEntity<List<MemberResponse>> getAllMembers() {
+    List<MemberResponse> members = memberService.findAll();
+
+    return ResponseEntity.ok(members);
+  }
+
+  // 3. 단건 조회
+  // 상태반환을 ok, not found 2가지 상태로 함
+  // try-catch가 되는 이유는 service에서 throws를 던졌기 때문
+  @GetMapping("/{id}")
+  public ResponseEntity<MemberResponse> getMemberById(@PathVariable("id") Long id) {
+    try {
+      MemberResponse foundMember = memberService.findById(id);
+      return ResponseEntity.ok(foundMember);
+
+    } catch (Exception e) {
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  // 4. 회원 수정
+  @PutMapping("/{id}")
+  public ResponseEntity<MemberResponse> updateMember(
+      @PathVariable("id") Long id,
+      @RequestBody MemberRequest request) {
+    try {
+      MemberResponse updatedMember = memberService.update(request, id);
+
+      return ResponseEntity.ok(updatedMember);
+    } catch (Exception e) {
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  // 5. 회원 삭제
+  // 제네릭에서 아무것도 안 보낼때는 Void를 사용한다.
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteMember(@PathVariable("id") Long id) {
+    try {
+      memberService.deleteById(id);
+
+      return ResponseEntity.noContent().build();
+    } catch (Exception e) {
+      return ResponseEntity.notFound().build();
+    }
   }
 }
