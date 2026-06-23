37f543678c6a8c629d84e5874982a765b798cfb6
perish95 <perish95@naver.com>
2026-06-23 10:20:33 +0900

    260622 Spring Security

---
+++ ex11_token_auth/react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import AuthService from './AuthService'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <AuthService/>
+  )
+}
+
+export default App
